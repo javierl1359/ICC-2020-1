17516964cf2a4d08231ae7811570c977dfc6d6cd
javierl1359 <javierl@ciencias.unam.mx>
2019-08-20 10:32:02 -0500

    practicas

---
+++ practica01/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+	     "openjdk version \"11.0.2\" 2019-01-15");
+	          System.out.println(
+				     "OpenJDK Runtime Environment 18.9 (build 11.0.2+9)");
+		  System.out.println(
+				     "OpenJDK 64-Bit Server VM 18.9 (build 11.0.2+9, mixed mode)");
+		  System.out.println(
+				     "");
+		  System.out.println(
+				     "Linux localhost.localdomain 5.0.9-301.fc30.x86_64 #1 SMP Tue Apr 23 23:57:35 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux");
+ }
+  }
