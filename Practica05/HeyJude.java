23afe79bfce8b15ae73372d41993734db67af7d3
javierl1359 <53274696+javierl1359@users.noreply.github.com>
2019-09-03 12:01:19 -0500

    Add files via upload

---
+++ Practica05/HeyJude.java
@@ -0,0 +1,28 @@
+public class HeyJude{
+    public static void main(String[] args){
+	String Jude = "hey Jude";
+	String Dont = "dont";
+	String Makem = "make it bad";
+	String Beafraid = "be afraid";
+	String Letm  = "let me down";
+	String Takesad = "take a sad song and make it better";
+	String Youwere = "you were made to go out ande get her";
+	String Youhave = "you have found her now go ande get her";
+	String Rememberto = "remember to";
+	String Lether = "let her into your heart";
+	String Letunder =" let her under your skin";
+	String Theny = "then you";
+	String Canst = "can start";
+	String Beg = "begin";
+	String Tomakeit = "to make it better";
+	String Better = "better";
+	String Na = "na";
+	int s = 0;
+	while(s <= 7){
+	    s++;
+	    if(s==0 || s<=1 || s<=3 || s<=5) {
+		System.out.print(Jude);
+	    }	
+	}
+    }
+} 
